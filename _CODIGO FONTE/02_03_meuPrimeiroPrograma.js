abf5a356dfbc19127d6b4f4dd3a65ae19ab5f812
Lucas Longo <67756611+lucaslongolinkedin@users.noreply.github.com>
2024-01-24 15:50:19 +0000

    new exercise files

---
+++ _CODIGO FONTE/02_03_meuPrimeiroPrograma.js
@@ -0,0 +1,8 @@
+var a = 1;
+console.log(a);
+
+var b = 2; 
+console.log(b);
+
+var c = a + b;
+console.log(c);
---
+++ _CODIGO FONTE/02_04_usandoIA.js
@@ -0,0 +1,10 @@
+// tenho a e b, se a for maior que b, imprima "a é maior que b", senão, imprima "b é maior que a"
+var a = 1;
+console.log(a);
+var b = 2;
+console.log(b);
+if (a > b) {
+    console.log("a é maior que b");
+} else {
+    console.log("b é maior que a");
+}
